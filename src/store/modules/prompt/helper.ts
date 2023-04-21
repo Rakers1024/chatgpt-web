0aa144de89b12bd4b7e26a7b55e19752a37c7afa
Rakers1024 <1139991371@qq.com>
2023-04-21 14:48:59 +0800

    feat: 默认导入自己的参数

--- src/store/modules/prompt/helper.ts
+++ src/store/modules/prompt/helper.ts
@@ -2,15 +2,33 @@ import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'promptStore'
 
+import ChengzzChatGPTPrompt from '../../../../public/ChengzzChatGPTPrompt.json'
+
 export type PromptList = []
 
 export interface PromptStore {
   promptList: PromptList
 }
 
+export function defaultSetting(): PromptStore {
+  //数据处理
+  let jsonData = ChengzzChatGPTPrompt
+    if ('act' in jsonData[0] && 'prompt' in jsonData[0]) {
+      jsonData = jsonData.map((item: { act: string; prompt: string }) => {
+        return {
+          key: item.act,
+          value: item.prompt,
+        }
+      }) as PromptList
+    }
+  return { 
+    promptList: jsonData as PromptList
+  }
+}
+
 export function getLocalPromptList(): PromptStore {
   const promptStore: PromptStore | undefined = ss.get(LOCAL_NAME)
-  return promptStore ?? { promptList: [] }
+  return { ...defaultSetting(), ...promptStore }
 }
 
 export function setLocalPromptList(promptStore: PromptStore): void {
